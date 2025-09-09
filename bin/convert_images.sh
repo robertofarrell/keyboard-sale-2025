3f7ef42041e1b6e27c62f072f7a4363f973a0762
Robert O'Farrell <rob@easyagile.com>
2025-09-09 13:10:06 +1000

    Add hhkb

--- bin/convert_images.sh
+++ bin/convert_images.sh
@@ -1,2 +1,10 @@
-sips -s format jpeg -s formatOptions high -s dpiWidth 72 -s dpiHeight 72 -m '/System/Library/ColorSync/Profiles/sRGB Profile.icc' -Z 400 *.HEIC --out ./thumbs/ 
+#!/bin/bash
+mkdir -p ./thumbs
+mkdir -p ./full
+
+# Convert all HEIC images in the current directory to JPEG format with specified options
+# Create thumbnails with max dimension 400px
+# Create resized images with max dimension 1600px
+# Using sRGB color profile and 72 DPI
+sips -s format jpeg -s formatOptions high -s dpiWidth 72 -s dpiHeight 72 -m '/System/Library/ColorSync/Profiles/sRGB Profile.icc' -Z 400 *.HEIC --out ./thumbs/
 sips -s format jpeg -s formatOptions high -s dpiWidth 72 -s dpiHeight 72 -m '/System/Library/ColorSync/Profiles/sRGB Profile.icc' -Z 1600 *.HEIC --out ./resized/
